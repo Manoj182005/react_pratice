5195fcbeb6c04faf2713c278cdbd1d251735776d
Manoj Patel BM <manojpatelbm182005@gmail.com>
2025-10-07 21:31:15 +0530

    Updated my work for today

---
+++ asynchronous.js
@@ -0,0 +1,13 @@
+function timer() {
+  for (let i = 1; i <= 5; i++) {
+      setTimeout(
+          function () {
+              //Write your code here...
+              console.log(i);
+
+          }, i * 1000
+    );
+    console.log("Hello ninjas!");
+  }
+}
+timer();
